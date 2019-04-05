af5521438be21f032c9b610f9c93d67c1b12a9e7
Jakob Bach <jakob.bach@kit.edu>
2019-04-05 16:39:21 +0200

    Add scoring code
    Scores the students' predictions, besides performing sanity checks.

--- README.md
+++ README.md
@@ -7,3 +7,10 @@
 The splits for the students can be created with the R script `SplitForStudents.R`.
 For this purpose, the DMC file `train.csv` has to be placed in a folder `data/` (relative to the script).
 No special R packages are required.
+
+## Scoring
+
+The predictions of the students can be scored with the R script `ScoreStudents.R`.
+The path to the prediction files has to be set for each scoring procedure (team and week).
+If the naming scheme of the prediction files or the path/naming scheme of the ground truth (as defined in `SplitForStudents.R`) changes, further adaptations might be necessary.
+No special R packages are required.
---
+++ ScoreStudents.R
@@ -0,0 +1,43 @@
+source("UtilityFunctions.R")
+
+# Things that have to be adapted per week and group:
+dateString <- "2019-04-14"
+predictionBasePath <- paste0("../Submissions/", dateString, "/Gryffindor/")
+
+# Get names of split files
+truthFiles <- list.files("data/", pattern = "split-.*-labels-.*\\.csv", full.names = TRUE)
+if (length(truthFiles) == 0) {
+  stop("The ground truth files seem to have disappeared.")
+}
+if (length(list.files(predictionBasePath, pattern = "\\.csv")) != length(truthFiles)) {
+  stop("Number of prediction and ground truth files differ.")
+}
+
+score <- 0 # will be averaged over splits
+for (truthFileName in truthFiles) {
+  seedString <- regmatches(truthFileName, regexpr("[0-9]+.csv$", truthFileName))
+  predictionFileName <- list.files(predictionBasePath, full.names = TRUE,
+      pattern = paste0(dateString, "-prediction-", seedString))
+  if (length(predictionFileName) != 1) {
+    stop(paste0("Zero or multiple matching prediction files found for \"", seedString, "\"."))
+  }
+  groundTruth <- read.csv(truthFileName, sep = "+") # causes files with row names to fail later
+  prediction <- read.csv(predictionFileName)
+  if (nrow(prediction) != nrow(groundTruth)) {
+    stop("Number of observations wrong.")
+  }
+  if (ncol(prediction) != ncol(groundTruth)) {
+    stop("Number of columns wrong.")
+  }
+  if (colnames(prediction) != colnames(groundTruth)) {
+    stop("Column name wrong.")
+  }
+  if (!is.numeric(prediction$fraud)) {
+    stop("Data type wrong.")
+  }
+  if (any(prediction$fraud != 0 & prediction$fraud != 1)) {
+    stop("Additional class labels.")
+  }
+  score <- score + dmcScore(actual = groundTruth$fraud, prediction = prediction$fraud)
+}
+print(score / length(truthFiles))
