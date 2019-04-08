7aa1e7c6b3f686602a5cb943144ee4796051f4f6
Jakob Bach <jakob.bach@kit.edu>
2019-04-08 09:27:56 +0200

    Add feature engineering
    Compute useful combinations of eight of the existing features (ignoring
    trust level for the start). Does improve prediction a lot, in particular
    the total number of items (which is the only "total" not given as original
    feature).

--- UtilityFunctions.R
+++ UtilityFunctions.R
@@ -10,3 +10,54 @@ xgbDMCScore <- function(preds, dtrain) {
               value = dmcScore(actual = xgboost::getinfo(dtrain, "label"),
                                prediction = as.integer(preds >= 0.5))))
 }
+
+# Systematic features engineering. Creates all features from four groups (total,
+# per value, per time, per items), while the original data have five "total"
+# features, two "PerTime" and one "perItems" and are not consistent in naming.
+# Currently we ignore the "trustLevel".
+engineerFeatures <- function(dataset) {
+  require(data.table)
+  if (!is.data.table(dataset)) {
+    dataset <- data.table(dataset)
+  } else {
+    dataset <- copy(dataset) # do not modify original
+  }
+  
+  # "total" (= absolute value) features (mostly already in data)
+  setnames(dataset, old = "totalScanTimeInSeconds", new = "Time_total")
+  setnames(dataset, old = "grandTotal", new = "Value_total")
+  # Total number also with lineItemVoids / lineItemVoidsPerPosition, but then NaNs
+  # because sometimes no voids (else same result); round because slight numerical
+  # imprecision (but should be whole numbers)
+  dataset[, Items_total := as.integer(round(scannedLineItemsPerSecond * Time_total))]
+  setnames(dataset, old = "lineItemVoids", new = "ItemVoids_total")
+  setnames(dataset, old = "scansWithoutRegistration", new = "ScansWithoutReg_total")
+  setnames(dataset, old = "quantityModifications", new = "QuantityMod_total")
+  dataset[, Intervention_total := as.integer(ItemVoids_total + ScansWithoutReg_total + QuantityMod_total)]
+  
+  # "perValue" features (relative to grand total value)
+  dataset[, Time_perValue := Time_total / Value_total]
+  dataset[, Items_perValue := Items_total / Value_total]
+  dataset[, ItemVoids_perValue := ItemVoids_total / Value_total]
+  dataset[, ScansWithoutReg_perValue := ScansWithoutReg_total / Value_total]
+  dataset[, QuantityMod_perValue := QuantityMod_total / Value_total]
+  dataset[, Intervention_perValue := Intervention_total / Value_total]
+  
+  # "perTime" features (relative to total scan time)
+  setnames(dataset, old = "valuePerSecond", new = "Value_perTime")
+  setnames(dataset, old = "scannedLineItemsPerSecond", new = "Items_perTime")
+  dataset[, ItemVoids_perTime := ItemVoids_total / Time_total]
+  dataset[, ScansWithoutReg_perTime := ScansWithoutReg_total / Time_total]
+  dataset[, QuantityMod_perTime := QuantityMod_total / Time_total]
+  dataset[, Intervention_perTime := Intervention_total / Time_total]
+  
+  # "perItem" features (relative to total number of scanned items)
+  dataset[, Value_perItems := Value_total / Items_total]
+  dataset[, Time_perItems := Time_total / Items_total]
+  setnames(dataset, old = "lineItemVoidsPerPosition", new = "ItemVoids_perItems")
+  dataset[, ScansWithoutReg_perItems := ScansWithoutReg_total / Items_total]
+  dataset[, QuantityMod_perItems := QuantityMod_total / Items_total]
+  dataset[, Intervention_perItems := Intervention_total / Items_total]
+  
+  return(dataset)
+}
