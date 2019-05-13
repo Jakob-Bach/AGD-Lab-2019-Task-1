2c6cbce5df18ad74c88dc50eb6aed34f13e2fc68
Jakob Bach <jakob.bach@kit.edu>
2019-05-13 11:05:12 +0200

    Handle NAs in file combination
    If there are NAs in the fraud predictions, replace them with zero.

--- CombinePredictions.R
+++ CombinePredictions.R
@@ -6,8 +6,11 @@ dir.create(outputDir, showWarnings = FALSE)
 for (predictionFileName in list.files(inputDirs[1], pattern = "prediction-[0-9]+\\.csv")) {
   fileNameWithoutTeam <- gsub("^[A-za-z]+", "", predictionFileName)
   allPredictionFiles <- list.files(inputDirs, fileNameWithoutTeam, full.names = TRUE)
-  predictionsList <- lapply(allPredictionFiles, function(teamPredictionFile)
-    read.csv(teamPredictionFile, sep = "+", quote = "")$fraud)
+  predictionsList <- lapply(allPredictionFiles, function(teamPredictionFile) {
+    fraudPrediction <- read.csv(teamPredictionFile, sep = "+", quote = "")$fraud
+    fraudPrediction[is.na(fraudPrediction)] <- 0
+    return(fraudPrediction)
+  })
   prediction <- Reduce("+", x = predictionsList)
   prediction <- data.frame(fraud = as.integer(prediction  > length(inputDirs) / 2)) # majority
   write.csv(prediction, file = paste0(outputDir, "Combined", fileNameWithoutTeam),
